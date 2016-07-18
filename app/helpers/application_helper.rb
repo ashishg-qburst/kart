15967b649aee2503e302e312b7d6e0adb80a07ae
ash <ashishg@qburst.com>
2016-07-18 17:51:43 +0530

    Route products index and show actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def full_title(page_title = '')
     if page_title.empty?
-      base_title
+      "Kart"
     else
       "#{page_title} | Kart"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'products' => 'products#index'
+
+  resources :products
+
   devise_for :users
   
   root  'static#home'
