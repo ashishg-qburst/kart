cab022503c565a21fd493589d39e6442e7a081a9
ash <ashishg@qburst.com>
2016-08-05 12:25:09 +0530

    Validate product price - greater than 0

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ActiveRecord::Base
   
   validates :name, presence: true
   validates :brand, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   
   ratyrate_rateable "product"
 
