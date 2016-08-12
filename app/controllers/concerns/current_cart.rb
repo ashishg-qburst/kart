3bc926aff786d600fd50cd4707d6ac3d0818524e
ash <ashishg@qburst.com>
2016-08-12 15:38:46 +0530

    Revert slim cart

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,6 +4,11 @@ module CurrentCart
   private
     def set_cart
       @cart = Cart.find(session[:cart_id])
+      @cart_items = @cart.items.all
+      @cart_products = Array.new
+      @cart_items.each do |i|
+        @cart_products.push(i.product.id)
+      end
       rescue ActiveRecord::RecordNotFound
         @cart = Cart.create
         session[:cart_id] = @cart.id
