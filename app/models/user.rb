b758c7e95a943c6c73b6a8854b4d51483ba56f15
ash <ashishg@qburst.com>
2016-08-16 16:53:51 +0530

    Add user, product model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
 
   ratyrate_rater
 
+  validates :name, presence: true, length: { maximum: 50 }
+  
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @product = Product.new(name: "product name", brand: "brand name",
+      price: 10)
+  end
+
+  test "should be valid" do
+    assert @product.valid?
+  end
+
+  test "name should be present" do
+    @product.name = ""
+    assert_not @product.valid?
+  end
+
+  test "brand should be present" do
+    @product.brand = ""
+    assert_not @product.valid?
+  end
+
+  test "price should be present" do
+    @product.price = ""
+    assert_not @product.valid?
+  end
+
+  test "price should be greater than 0" do
+    @product.price = 0
+    assert_not @product.valid?
+    @product.price = 10
+    assert @product.valid?
+  end
 end
